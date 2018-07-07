49352448ded41a4c153fa98e8ee55a8edf57642d
Gradnok <gradnok@gmail.com>
2018-07-07 16:34:25 +0200

    initialisation

---
+++ springbootfirststeps/src/main/java/de/softaz/springbootfirststeps/SpringbootfirststepsApplication.java
@@ -0,0 +1,12 @@
+package de.softaz.springbootfirststeps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfirststepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootfirststepsApplication.class, args);
+	}
+}
---
+++ springbootfirststeps/src/test/java/de/softaz/springbootfirststeps/SpringbootfirststepsApplicationTests.java
@@ -0,0 +1,16 @@
+package de.softaz.springbootfirststeps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootfirststepsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
